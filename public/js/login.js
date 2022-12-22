51d7e128df7a60193f1cca29bbb7e8b3b49a2857
DummyWoke <ramseybanda2@gmail.com>
2022-12-22 10:28:49 -0500

    another test

--- public/js/login.js
+++ public/js/login.js
@@ -1,36 +1,9 @@
-//var myAlert = document.getElementById('myAlert')
-//myAlert.addEventListener('closed.bs.alert', function () {
-  // do something, for instance, explicitly move focus to the most appropriate element,
-  // so it doesn't get lost/reset to the start of the page
-  // document.getElementById('...').focus()
-//})
-
-
-console.log("testing")
-
-var UserSignUp = $("#UserSignUpForm")
-var UserLogin = $("#UserLoginForm")
-var NoteForm = $("#NoteForm")
-
-var Username = $("#Login-modal-user")
-var Password = $("#Login-modal-password")
-
-
-console.log(Username)
-$(UserLogin).submit(function(event){
-    event.preventDefault()
-    var user = Username.val()
-    var password = Password.val()
-    console.log(user)
-    console.log([password])
-  });
-
-  const loginFormHandler = async (event) => {
+const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
-    const user = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const user = document.querySelector('#Login-modal-user').value.trim();
+    const password = document.querySelector('#Login-modal-password').value.trim();
   
     if (user && password) {
       // Send a POST request to the API endpoint
@@ -52,14 +25,13 @@ $(UserLogin).submit(function(event){
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const user = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const user = document.querySelector('#Signup-modal-user').value.trim();
+    const password = document.querySelector('#Signup-modal-password').value.trim();
   
-    if (name && user && password) {
+    if (user && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, user, password }),
+        body: JSON.stringify({ user, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
