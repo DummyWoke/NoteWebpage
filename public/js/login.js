3c2f7540c7f5cc34f907761af7d77678cbcd5851
DummyWoke <ramseybanda2@gmail.com>
2022-12-22 12:05:20 -0500

    Update

--- public/js/login.js
+++ public/js/login.js
@@ -37,7 +37,7 @@ const loginFormHandler = async (event) => {
   };
   
   document
-    .querySelector('.login-form')
+    .querySelector('#UserLoginForm')
     .addEventListener('submit', loginFormHandler);
   
   document
