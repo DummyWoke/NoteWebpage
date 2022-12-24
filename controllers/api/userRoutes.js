4e2dd9c750485e11f04ecfdb8c031d226611c7af
DummyWoke <ramseybanda2@gmail.com>
2022-12-24 00:54:04 -0500

    Final

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,10 +3,6 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-
-    console.log("gotuserpostrequest")
-    
-
     const userData = await User.create(req.body);
 
     req.session.save(() => {
@@ -21,38 +17,38 @@ router.post('/', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  console.log("gotuserloginrequest")
-  console.log("req.body= ",req.body);
-  console.log("username= ",req.body.username);
   try {
-    const userData = await User.findOne({ where: { username: req.body.user } });
+    const userData = await User.findOne({ where: { user: req.body.username } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+        .json({ message: 'Incorrect user or password, please try again' });
       return;
     }
-
-    console.log("founduser = ", userData)
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+        .json({ message: 'Incorrect user or password, please try again' });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.logged_in = true;;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.post('/logout', (req, res) => {
-
-
-  console.log("gotuserpostlogout")
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
